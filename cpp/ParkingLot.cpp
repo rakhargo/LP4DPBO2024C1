6fbf3b3b3f5690980a078fd5b3355961007a553f
rakhargo <117525800+rakhargo@users.noreply.github.com>
2024-03-04 10:31:34 +0700

    Add files via upload

--- cpp/ParkingLot.cpp
+++ cpp/ParkingLot.cpp
@@ -1,3 +1,10 @@
+/*Saya Rakha Dhifiargo Hariadi
+ NIM 2209489 mengerjakan soal 
+ Latihan praktikum 4 dalam mata
+ kuliah DPBO
+ untuk keberkahanNya maka saya tidak
+ melakukan kecurangan seperti 
+ yang telah dispesifikasikan. Aamiin.*/ 
 #pragma once
 #include <bits/stdc++.h>
 #include "Vehicle.cpp" // include the Vehicle class file
